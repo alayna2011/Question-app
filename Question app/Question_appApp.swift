241bddb3121a8d4aab9ad896156f437defb4102a
alayna2011 <alaynachia@gmail.com>
2024-07-16 10:14:15 -0700

    Initial Commit

---
+++ Question app/Question_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_appApp.swift
+//  Question app
+//
+//  Created by Alayna Yee-Huan Chia on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Question_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
